26bbbdc1f9e7f582f551bc22c469e6b1cf63979e
Patrick Dressel <patrick@hoox.software>
2024-06-26 09:32:58 +0200

    update doc
    scan all datatypes
    create links for datatypes in project
    some bugfixes

--- AutoDoc.ps1
+++ AutoDoc.ps1
@@ -4,61 +4,182 @@ cls
 $strProject = 'C:\Temp\src' # change this lines for your Project
 $strExport  = 'C:\Temp\doc' # add folder for the md-files
  
-$RegExBegin = '<(Method|Action|Property|Get|Set) Name="'
-$RegexName  = '(?s).(.*?)'
-$RegexEnd   = '" Id='
-$Regex      = "(?s)(?=($RegExBegin))($RegExName)(?=($RegExEnd))"
+$RegExBegin           = '<(Method|Action|Property|Get) Name="'
+$RegexName            = '(?s).(.*?)'
+$RegexEnd             = '" Id='
+$Regex                = "(?s)(?=($RegExBegin))($RegExName)(?=($RegExEnd))"
  
-$RegexReturn     = '(?<Method>%Name) *: *(?<Return>\S*)'
-$RegexVariable   = '(?<Variable>\S*)\s*: *(?<Type>\S*)'
-$RegexComment    = '(?<Start>\(\*\* *)(?<Value>.*?)(?<End>\*\*\))'
-$RegexComment    = '(?<Start>\/\/ *)(?<Value>.*)'
+$RegexReturn          = '(?<Method>%Name) *: *(?<Return>\S*)'
+$RegexComment         = '(?<Start>\/\/ *)(?<Value>.*)'
 $RegexCommentBegin    = '(?<Start>\(\*\* +)(?<Line>.*)'
 $RegexCommentEnd      = '(?<Line>.*?)(?<End> *\*\*\))'
+$RegexNumber          = '(?<Index>[0-9][0-9][0-9]*_)(?<Filename>.*)'
  
+$RegExType           = '(?<KEY>TYPE ).*?(?<Name>[A-z_]*)(?<Ending>.*:)'
+$RegExElement        = '(?<Variable>\S*)\s*: *(?<Type>\S*)'
+$RegExEnum           = '(?<Variable>\S*)\s*(?<Init>:= *(?<Value>[0-9]))*(?<End>,)'
+$RegexVariable       = '(?<Variable>\S*)\s*:\s*(?<Type>[A-z_.]*)'
+ 
+$overview = @();
+$TypesOverview = @();
+ 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Entrypoint for documentation
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function New-Documentation
 {
     param(
         [string] $Path,
-        [string] $Filter,
+        [string] $FileType,
         [string] $Destination,
+        [switch] $Subfolder,
         [switch] $Structured
     )
  
     $index = 0
+    $Filter = "*.$FileType"
+    $strSubfolder = ""
  
-    Get-ChildItem -Path $Path -Recurse -Filter "*.tcPou" | % {
+    if ($FileType.contains("DUT"))
+    {
+        Write-Host "Create datatypes..."
+        if ($Subfolder)
+        {
+            $strSubfolder = "Types"
+        }
+    }
+    if ($FileType.contains("POU"))
+    {
+        Write-Host "Create sources..."
+        if ($Subfolder)
+        {
+            $strSubfolder = "Source"
+        }
+    }
+    if ($FileType.contains("GVL"))
+    {
+        Write-Host "Create variables..."
+        if ($Subfolder)
+        {
+            $strSubfolder += "\Variables"
+        }
+    }
+ 
+    Get-ChildItem -Path $Path -Recurse -Filter $Filter | % {
  
       $index++
       $strPath = Split-Path -Parent $_.fullname
       $strFile = Split-Path -Leaf $_.fullname
  
-      $Content = Read-SourceFile -Path $strPath -File $strFile
+      if ($FileType.contains("DUT"))
+      {
+        $Content = Read-TypeFile -Path $strPath -File $strFile
+      }
+      if ($FileType.contains("POU"))
+      {
+        $Content = Read-SourceFile -Path $strPath -File $strFile
+      }
+      if ($FileType.contains("GVL"))
+      {
+        $Content = Read-SourceFile -Path $strPath -File $strFile
+      }
+     
+      $FileNew = ""
     
       if ($index -lt 10)
       {
-        $FileNew = "0" + $index.ToString() + "_" + $strFile.Replace("TcPOU", "md")
+        $FileNew = "0"
       }
-      else
-      {
-        $FileNew = $index.ToString() + "_" + $strFile.Replace("TcPOU", "md")
-      }
-    
  
-      $FolderNew = $Destination;
+      $FileNew += $index.ToString() + "_" + $strFile.Replace($FileType, "md")
+    
+     
+      $FolderNew = "$Destination\$strSubfolder";
  
       if ($Structured -eq $true)
       {
           $FolderNew += $strPath.Replace($Path,"")
       }
  
-      New-Item -Path $FolderNew -Name $FileNew -Force
-    
+      Write-Host "Create $FileNew"
+ 
+      $temp = New-Item -Path $FolderNew -Name $FileNew -Force
+ 
+      if ($FileType.contains("DUT"))
+      {
+        $global:TypesOverview += "$strSubfolder\$FileNew";
+      }
+      if ($FileType.contains("POU"))
+      {
+        $global:SourceOverview += "$strSubfolder\$FileNew";
+      }
+     
       Set-Content -Path "$FolderNew\$FileNew" -Value $Content -Encoding UTF8   
  
     }
+ 
 }
  
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Entrypoint for documentation
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function New-Overview
+{
+ 
+    param(
+        [string] $Destination
+    )
+ 
+    Write-Host "Create Overview"
+ 
+    $temp = New-Item -Path $Destination -Name "00_Overview.md" -Force
+ 
+    $strContent = "[[_TOC_]]`n`n"
+    $strContent += "# Functionblocks`n`n"
+    $index = 1
+ 
+    foreach($element in $global:SourceOverview)
+    {
+      $strFunction = $element
+      if ($element -match $RegexNumber)
+      {
+         $strPath = Split-Path -Parent $strFunction
+ 
+         $strFunction = $strFunction.Replace($Matches["Index"], "")
+         $strFunction = $strFunction.Replace(".md", "")
+         $strFunction = $strFunction.Replace("$strPath\", "")
+      }
+ 
+      $strContent += " $index. [$strFunction]($element)`n"
+      $index++
+    }
+ 
+    $strContent += "`n`n# Datatypes`n`n"
+    $index = 1
+ 
+    foreach($element in $global:TypesOverview)
+    {
+      $strFunction = $element
+      if ($element -match $RegexNumber)
+      {
+         $strPath = Split-Path -Parent $strFunction
+ 
+         $strFunction = $strFunction.Replace($Matches["Index"], "")
+         $strFunction = $strFunction.Replace(".md", "")
+         $strFunction = $strFunction.Replace("$strPath\", "")
+      }
+ 
+      $strContent += " $index. [$strFunction]($element)`n"
+      $index++
+    }
+ 
+    Set-Content -Path "$Destination\00_Overview.md" -Value $strContent -Encoding UTF8
+}
+ 
+ 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Get content of TcPOU File
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function Read-SourceFile
 {
     param(
@@ -66,8 +187,7 @@ function Read-SourceFile
         [string] $File      
     )
  
-    Write-Host "Create $FileNew"
- 
+   
     $strContent = "[[_TOC_]]`n`n"
     $strContent += "## " + $File.Replace(".TcPOU", "") + "`n`n"
  
@@ -108,7 +228,6 @@ function Read-SourceFile
                 $strMethodName = $strMethodName.Replace("<Action Name=`"","")
                 $strMethodName = $strMethodName.Replace("<Property Name=`"","")
                 $strMethodName = $strMethodName.Replace("<Get Name=`"","")
-                $strMethodName = $strMethodName.Replace("<Set Name=`"","")
                 $strContent += "### " + $strMethodType + " " + $strMethodName + "  `n"
               }
  
@@ -177,7 +296,7 @@ function Read-SourceFile
                 {
                     if ($strLine -match $RegexCommentEnd)
                     {                
-                        $Description = $false                                      
+                        $Description = $false                                       
                     }
                     else
                     {
@@ -193,7 +312,7 @@ function Read-SourceFile
                     }
                 }
  
-                if ($strLine.Contains("VAR") -eq $true)
+                if ($strLine.Contains("VAR ") -eq $true)
                 {
                     $VarLocal = $true;
                 }
@@ -234,6 +353,9 @@ function Read-SourceFile
     $strContent 
 }
  
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Get variables in TcPOU
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function Read-Variables()
 {
     param(
@@ -251,9 +373,158 @@ function Read-Variables()
     {                  
         $strData += "|"+ $Matches["Variable"] + " |" + $Matches["Type"] + " |"+ $strComment + "| `n"
         $strData = $strData.Replace(';', '')
+ 
+        if ($global:TypesMap.ContainsKey($Matches["Type"]))
+        {
+          $strLink = "[" + $Matches["Type"] + "](" + $global:TypesMap[$Matches["Type"]] + ")"
+          $strData = $strData.Replace($Matches["Type"], $strLink)
+        }
     }
  
     $strData
 }
  
-New-Documentation -Path $strProject -Filter "*.tcPOU" -Destination $strExport #-Structured
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Get content of tcDUT File
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function Read-TypeFile
+{
+    param(
+        [string] $Path,
+        [string] $File      
+    )
+ 
+    Write-Host "Check Typefile $File "
+    $Declaration = $false
+    $strSource = Get-Content -Path "$Path\$File"
+ 
+    foreach($strLine in $strSource)
+    {
+ 
+        if ($strLine -match $RegExType)
+        {
+            Write-Host $strLine
+            $Declaration = $true
+ 
+            $strContent = "Type : " + $Matches["Name"] + "`n`n"
+           
+            if ($strSource.Contains("STRUCT"))
+            {
+                $strContent += "|Name |Type |Comment| `n"
+            }
+            else
+            {
+                $strContent += "|Name |Initvalue |Comment| `n"
+            }
+ 
+            $strContent += "|---- |---- |----   | `n"
+        }
+        else
+        {
+            if ($Declaration -eq $true)
+            {
+                if (-not $strLine.Contains("EXTENDS") -and -not $strLine.Contains("STRUCT"))
+                {
+                    if ($strLine -match $RegexEnum)
+                    {
+                        $strContent += "|%1 |%2 |%3   | `n"                                               
+                        $strContent = $strContent.Replace("%1", $Matches["Variable"])
+                        $strContent = $strContent.Replace("%2", $Matches["Value"])
+                 
+                    }
+                    else
+                    {
+                        if ($strLine -match $RegexVariable)
+                        {                              
+                            $strTemp = "|%1 |%2 |%3   | `n"
+                            $strTemp = $strTemp.Replace("%1", $Matches["Variable"])
+                            $strTemp = $strTemp.Replace("%2", $Matches["Type"])
+                         
+                            if ($global:TypesMap.ContainsKey($Matches["Type"]))
+                            {
+                              $strLink = "[" + $Matches["Type"] + "](" + $global:TypesMap[$Matches["Type"]] + ")"
+                              $strLink = $strLink.Replace("Types\", "")
+                              $strTemp = $strTemp.Replace($Matches["Type"], $strLink)
+                            }
+                        
+                            $strContent += $strTemp                                                
+                        }
+                    }
+                    if ($strLine -match $RegexComment -or $strLine -match $RegexLineComment)
+                    {                              
+                         $strContent = $strContent.Replace("%3", $Matches["Value"])          
+                    }
+ 
+                    if ($strContent)
+                    {
+                        $strContent = $strContent.Replace("%3", "")
+                    }  
+                }
+ 
+        
+            }
+ 
+            if ($strLine.Contains("END_TYPE") )
+            {
+                $Declaration = $false
+                $strContent
+                return
+            }
+           
+                  
+        }
+       
+    }
+ 
+    $strContent
+ 
+}
+ 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Build type map for later resolution
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function Create-TypeMap
+{
+    param(
+        [string] $Path,
+        [switch] $Subfolder      
+    )
+ 
+    $index = 0
+    Write-Host "Create global Type map..."
+ 
+    Get-ChildItem -Path $Path -Recurse -Filter "*.TcDUT" | % {
+ 
+      $index++
+      $strPath = Split-Path -Parent $_.fullname
+      $strFile = Split-Path -Leaf $_.fullname
+ 
+      #Write-host $strFile
+ 
+        $strSource = Get-Content -Path "$strPath\$strFile"
+ 
+        $strSubfolder = ""
+        if ($Subfolder)
+        {
+            $strSubfolder = "Types"
+        }
+ 
+        foreach($strLine in $strSource)
+        {
+            if ($strLine -match $RegExType)
+            {
+                $global:TypesMap[$Matches["Name"]] = $strSubfolder + "\" + $index.ToString() + "_" + $strFile.Replace("TcDUT", "md")
+            }
+       
+        }
+ 
+    }
+}
+ 
+Create-TypeMap -Path $strProject -Subfolder
+ 
+New-Documentation -Path $strProject -FileType "TcDUT" -Destination "$strExport" -Subfolder
+ 
+New-Documentation -Path $strProject -FileType "TcPOU" -Destination "$strExport" -Subfolder
+ 
+New-Overview -Destination $strExport
